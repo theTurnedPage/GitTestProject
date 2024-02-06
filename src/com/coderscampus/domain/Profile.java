6c0c892570605642d303d558c4d5730ea4314738
theTurnedPage <158675598+theTurnedPage@users.noreply.github.com>
2024-02-05 22:00:00 -0600

    Added a new profile domain object
    blah blah blah

---
+++ src/com/coderscampus/domain/Profile.java
@@ -0,0 +1,13 @@
+package com.coderscampus.domain;
+
+public class Profile {
+	private Integer id;
+	private String name;
+	private String url;
+	
+	public Integer getId() {
+		return id
+	}
+	
+	//blah blah blah more code
+}
